2bd9833003f060417054237788419f603657edaf
glbkstf <63748458+glbkstf@users.noreply.github.com>
2022-05-02 19:47:57 +0200

    Add files via upload

---
+++ indev/mip/display.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "defines.h"
+#include "display.h"
+
+/*void disp_pyramide(S_pyr pyramide[])
+{
+    printf("couleur : ");
+    for (int i=0; i<5; i++)
+        printf(" %d ", pyramide[i].couleur);
+    printf("\nvaleur :  ");
+    for (int i=0; i<5; i++)
+        printf(" %d ", pyramide[i].valeur);
+}
+
+void disp_piste(S_case piste[])
+{
+    printf("desert : ");
+    for (int i=0; i<16; i++)
+        printf("  %d  ", piste[i].desert);
+    printf("\nchamal : ");
+    for (int j=0; j<5; i++)
+        for (int i=0; i<16; i++)
+            printf("  %d  ", piste[i].chameaux[j]);
+}*/
