9870139f7f2911a5f742333b15d5133ce7306c4e
glbkstf <63748458+glbkstf@users.noreply.github.com>
2022-05-08 21:06:28 +0200

    Delete deplacnt directory

--- deplacnt/tirage_pyramide2.h
+++
@@ -1,7 +0,0 @@
-#ifndef TIRAGE_PYRAMIDE2_H_INCLUDED
-#define TIRAGE_PYRAMIDE2_H_INCLUDED
-
-//mélange une vouvelle pyramide
-void tirage_pyramide(S_pyr pyramide[]);
-
-#endif // TIRAGE_PYRAMIDE2_H_INCLUDED
