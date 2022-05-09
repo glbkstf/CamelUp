5f892a252f8713a3af3bf3add55d86cab9d641cb
glbkstf <63748458+glbkstf@users.noreply.github.com>
2022-05-09 20:43:28 +0200

    Add files via upload

---
+++ endgame/result_course.h
@@ -0,0 +1,6 @@
+#ifndef RESULT_COURSE_H_INCLUDED
+#define RESULT_COURSE_H_INCLUDED
+
+void result_course(S_joueur joueurs[], S_case piste[], int pos[], int pari[][2], int etat);
+
+#endif // RESULT_COURSE_H_INCLUDED
