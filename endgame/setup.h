6e77bfbbb82ceb9d68dc493c9384e2c4ba6f28e9
glbkstf <63748458+glbkstf@users.noreply.github.com>
2022-05-09 20:52:25 +0200

    Add files via upload

---
+++ endgame/setup.h
@@ -0,0 +1,8 @@
+#ifndef SETUP_H_INCLUDED
+#define SETUP_H_INCLUDED
+
+void setup(S_case piste[], S_joueur joueurs[], int nb_plyr);
+
+void setup_pari_course(int cartes[][2], int nb_plyr);
+
+#endif // SETUP_H_INCLUDED
